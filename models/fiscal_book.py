0b6a98e3f56e7a6684482a6201a9be5bdd050360
Mario Roberto <mrgomez.dev@gmail.com>
2024-05-01 12:24:40 -0600

    Add onchange in boolean

--- models/fiscal_book.py
+++ models/fiscal_book.py
@@ -1,8 +1,15 @@
 # -*- coding: utf-8 -*-
 
 from odoo import models, fields, api
+from odoo.exceptions import UserError
 
 class FiscalBook(models.Model):
     _inherit = 'fiscal.book'
 
     dte_annexes = fields.Boolean(string="DTE en Anexos")
+
+    @api.onchange('dte_annexes')
+    def _onchange_dte_annexes(self):
+        if self.dte_annexes:
+            # Lanza una alerta en el navegador
+            raise UserError("DTE en Anexos está activado. ¡Atención!")
